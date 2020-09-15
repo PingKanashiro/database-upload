3be40059fe68779f0c26333fcb608d8cce2fda56
Fernando Kanashiro <fernando_skanashiro@hotmail.com>
2020-09-15 15:06:28 -0300

    Project Database Upload

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,35 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.createQueryBuilder(
+      'transactions',
+    ).getMany();
+
+    const income = transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const totalIncome = income.reduce(
+      (total, { value }: { value: number }) => total + value,
+      0,
+    );
+
+    const outcome = transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const totalOutcome = outcome.reduce(
+      (total, { value }: { value: number }) => total + value,
+      0,
+    );
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 }
 
